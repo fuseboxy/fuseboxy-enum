db513c8fc34aebc0388bfe4fc0beb0d22c42a4b6
Henry Shum <henry@metaseit.com>
2017-03-20 18:01:24 +0800

    init repo

---
+++ app/controller/enum_controller.php
@@ -0,0 +1,46 @@
+<?php
+F::redirect('auth', !Auth::user());
+F::redirect(F::config('defaultCommand'), !Auth::activeUserInRole('SUPER,ADMIN'));
+
+
+// default type
+if ( !isset($_SESSION['enum_type']) ) {
+	$_SESSION['enum_type'] = R::getCell('SELECT type FROM enum ORDER BY type');
+}
+
+
+// change selected type (pass by url from layout)
+if ( isset($arguments['type']) ) {
+	$_SESSION['enum_type'] = $arguments['type'];
+}
+
+
+// config
+$scaffold = array(
+	'beanType' => 'enum',
+	'editMode' => 'inline',
+	'allowDelete' => Auth::activeUserInRole('SUPER'),
+	'layoutPath' => F::config('appPath').'view/enum/layout.php',
+	'listFilter' => array('type = ?', array($_SESSION['enum_type'])),
+	'listField' => array(
+		'id' => '7%',
+		'key|type' => '15%',
+		'value|remark' => '50%',
+		'seq' => '7%'
+	),
+	'fieldConfig' => array(
+		'id' => array(),
+		'type' => array('placeholder' => 'Type', 'readonly' => !Auth::activeUserInRole('SUPER'), 'default' => $_SESSION['enum_type']),
+		'key' => array('placeholder' => 'Key'),
+		'value' => array('placeholder' => 'Value'),
+		'remark' => array('placeholder' => 'Remark'),
+		'seq' => array('placeholder' => 'Seq')
+	),
+	'uploadBaseUrl' => F::config('baseUrl').'data/upload/',
+	'writeLog' => true,
+);
+
+
+// component
+$layout['width'] = 'full';
+include 'scaffold_controller.php';
---
+++ app/view/enum/layout.php
@@ -0,0 +1,37 @@
+<?php
+// tab layout config
+$tabLayout = array(
+	'style' => 'tab',
+	'position' => 'left',
+	'header' => '<h3>Settings</h3>',
+	'nav' => array(),
+);
+
+
+// tab : all existing types
+$types = R::getCol('SELECT DISTINCT type FROM enum ORDER BY type');
+foreach ( $types as $t ) {
+	$tabLayout['nav'][] = array(
+		'name' => ucwords( str_replace('_', ' ', strtolower($t) ) ),
+		'url' => F::url("{$fusebox->controller}&type={$t}"),
+		'active' => ( !empty($_SESSION['enum_type']) and $_SESSION['enum_type'] == $t )
+	);
+}
+
+
+// tab : new type
+$tabLayout['nav'][] = array(
+	'name' => '<em class="text-muted">+ New Setting</em>',
+	'url' => F::url("{$fusebox->controller}&type=(new)"),
+	'active' => ( empty($_SESSION['enum_type']) or $_SESSION['enum_type'] == '(new)' )
+);
+
+
+// tab layout
+ob_start();
+include F::config('appPath').'view/global/tab.php';
+$layout['content'] = ob_get_clean();
+
+
+// wrap by global layout
+include F::config('appPath').'view/global/layout.php';
