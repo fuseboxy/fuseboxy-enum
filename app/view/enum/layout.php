6bb56481dc3d419883a0fe748c6c65e8f37d8dd7
Henry Shum <henry.shum@cityu.edu.hk>
2019-09-23 15:13:05 +0800

    refactor enum layout

--- app/view/enum/layout.php
+++ app/view/enum/layout.php
@@ -1,30 +1,31 @@
 <?php
 // tab layout config
 $tabLayout = array(
-	'style' => 'tab',
+	'style' => 'tabs',
 	'position' => 'left',
 	'header' => '<h3>Settings</h3>',
-	'nav' => array(),
-);
-
-
-// tab : all existing types
-$types = R::getCol('SELECT DISTINCT type FROM enum ORDER BY type');
-foreach ( $types as $item ) {
-	$tabLayout['nav'][] = array(
-		'name' => ucwords( str_replace('_', ' ', strtolower($item) ) ),
-		'url' => F::url("{$fusebox->controller}&type={$item}"),
-		'active' => ( !empty($_SESSION['enumController__enumType']) and $_SESSION['enumController__enumType'] == $item ),
-		'remark' => R::count('enum', 'type = ? AND disabled = 0', array($item)),
-	);
-}
-
-
-// tab : new type
-$tabLayout['nav'][] = array(
-	'name' => '<em class="text-muted">+ New Setting</em>',
-	'url' => F::url("{$fusebox->controller}&type=(new)"),
-	'active' => ( empty($_SESSION['enumController__enumType']) or $_SESSION['enumController__enumType'] == '(new)' )
+	'nav' => call_user_func(function(){
+		$menu = array();
+		// existing types
+		$types = R::getCol('SELECT DISTINCT type FROM enum ORDER BY type');
+		foreach ( $types as $item ) {
+			$menus[] = array(
+				'name' => ucwords( str_replace('_', ' ', strtolower($item) ) ),
+				'url' => F::url( F::command('controller').'&type='.$item ),
+				'active' => ( !empty($_SESSION['enumController__enumType']) and $_SESSION['enumController__enumType'] == $item ),
+				'remark' => R::count('enum', 'type = ? AND disabled = 0', array($item)),
+			);
+		}
+		// new type
+		$menus[] = array(
+			'name' => '+ New Setting',
+			'url' => F::url( F::command('controller').'&type=(new)' ),
+			'active' => ( empty($_SESSION['enumController__enumType']) or $_SESSION['enumController__enumType'] == '(new)' ),
+			'linkClass' => 'font-italic text-muted',
+		);
+		// done!
+		return $menus;
+	}),
 );
 
 
