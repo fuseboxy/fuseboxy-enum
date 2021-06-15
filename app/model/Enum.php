136804b24ffdc4283aef7aa0f2c259f881d5b2ca
henrygotmojo <henrygotmojo@gmail.com>
2021-06-15 18:09:50 +0800

    rename methods and create alias methods for backward compatibility

--- app/model/Enum.php
+++ app/model/Enum.php
@@ -9,6 +9,104 @@ class Enum {
 
 
 
+	// alias methods (for backward compatibility)
+	public static function getAll  ($type, $key=null)                       { return self::all($type, $key); }
+	public static function getArray($type, $key=null, $all=false)           { return self::array($type, $key, $all); }
+	public static function getFirst($type, $all=false)                      { return self::first($type, $all); }
+	public static function getValue($type, $key, $returnKeyIfNotFound=true) { return self::value($type, $key, $returnKeyIfNotFound); }
+
+
+
+
+	/**
+	<fusedoc>
+		<description>
+			get all items (included disabled) of specific type
+		</description>
+		<io>
+			<in>
+			</in>
+			<out>
+				<structure name="~return~">
+					<string name="type" />
+					<string name="key" />
+					<string name="value" />
+					<string name="remark" />
+				</object>
+			</out>
+		</io>
+	</fusedoc>
+	*/
+	public static function all($type, $key=null) {
+		return self::get($type, $key, true);
+	}
+
+
+
+
+	/**
+	<fusedoc>
+		<description>
+			get multiple items as an array
+		</description>
+		<io>
+			<in>
+				<string name="$type" />
+				<string name="$key" optional="yes" comments="supposed to have wildcard" />
+				<boolean name="$all" optional="yes" default="false" comments="include disabled items when true" />
+			</in>
+			<out>
+				<structure name="~return~">
+					<string name="~enumKey~" value="~enumValue~" />
+				</structure>
+			</out>
+		</io>
+	</fusedoc>
+	*/
+	public static function array($type, $key=null, $all=false) {
+		$beans = self::get($type, $key, $all);
+		// check if multiple or single
+		if ( empty($key) or self::hasWildcard($key) ) {
+			return self::toArray($beans);
+		} elseif ( !empty($beans->id) ) {
+			return array( $beans->key => $beans->value );
+		} else {
+			return array();
+		}
+	}
+
+
+
+
+	/**
+	<fusedoc>
+		<description>
+			get first item of specific type
+		</description>
+		<io>
+			<in>
+				<string name="$type" />
+				<boolean name="$all" optional="yes" default="false" comments="include disabled items when true" />
+			</in>
+			<out>
+				<object name="~return~">
+					<string name="type" />
+					<string name="key" />
+					<string name="value" />
+					<string name="remark" />
+				</object>
+			</out>
+		</io>
+	</fusedoc>
+	*/
+	public static function first($type, $all=false) {
+		$beans = self::get($type, null, $all);
+		return !empty($beans) ? array_shift($beans) : ORM::new('enum');
+	}
+
+
+
+
 	/**
 	<fusedoc>
 		<description>
@@ -52,9 +150,9 @@ class Enum {
 			$filterParam[] = $key;
 		}
 		// order
-		$order = 'ORDER BY IFNULL(`seq`, 9999), `key` ASC ';
+		$order = 'ORDER BY IFNULL(`seq`, 99999), `key` ASC ';
 		// get multi records
-		if ( empty($key) or self::__hasWildcard($key) ) {
+		if ( empty($key) or self::hasWildcard($key) ) {
 			$result = ORM::get('enum', $filter.$order, $filterParam);
 		// or single value
 		} else {
@@ -75,116 +173,20 @@ class Enum {
 	/**
 	<fusedoc>
 		<description>
-			get first item of specific type
+			check whether the string has SQL wildcard character
 		</description>
 		<io>
 			<in>
-				<string name="$type" />
-				<boolean name="$all" optional="yes" default="false" comments="include disabled items when true" />
+				<string name="$str" example="home-%" />
 			</in>
 			<out>
-				<object name="~return~">
-					<string name="type" />
-					<string name="key" />
-					<string name="value" />
-					<string name="remark" />
-				</object>
+				<boolean name="~return~" />
 			</out>
 		</io>
 	</fusedoc>
 	*/
-	public static function getFirst($type, $all=false) {
-		$beans = self::get($type, null, $all);
-		return !empty($beans) ? array_shift($beans) : ORM::new('enum');
-	}
-
-
-
-
-	/**
-	<fusedoc>
-		<description>
-			get all items (included disabled) of specific type
-		</description>
-		<io>
-			<in>
-			</in>
-			<out>
-				<structure name="~return~">
-					<string name="type" />
-					<string name="key" />
-					<string name="value" />
-					<string name="remark" />
-				</object>
-			</out>
-		</io>
-	</fusedoc>
-	*/
-	public static function getAll($type, $key=null) {
-		return self::get($type, $key, true);
-	}
-
-
-
-
-	/**
-	<fusedoc>
-		<description>
-			get multiple items as an array
-		</description>
-		<io>
-			<in>
-				<string name="$type" />
-				<string name="$key" optional="yes" comments="supposed to have wildcard" />
-				<boolean name="$all" optional="yes" default="false" comments="include disabled items when true" />
-			</in>
-			<out>
-				<structure name="~return~">
-					<string name="~enumKey~" value="~enumValue~" />
-				</structure>
-			</out>
-		</io>
-	</fusedoc>
-	*/
-	public static function getArray($type, $key=null, $all=false) {
-		$beans = self::get($type, $key, $all);
-		// check if multiple or single
-		if ( empty($key) or self::__hasWildcard($key) ) {
-			return self::toArray($beans);
-		} elseif ( !empty($beans->id) ) {
-			return array( $beans->key => $beans->value );
-		} else {
-			return array();
-		}
-	}
-
-
-
-
-	/**
-	<fusedoc>
-		<description>
-			get value of specific enum item
-		</description>
-		<io>
-			<in>
-				<string name="$type" />
-				<string name="$key" />
-				<boolean name="$returnKeyIfNotFound" optional="yes" default="true" comments="return empty string otherwise" />
-			</in>
-			<out>
-				<string name="~return~" />
-			</out>
-		</io>
-	</fusedoc>
-	*/
-	public static function getValue($type, $key, $returnKeyIfNotFound=true) {
-		$result = self::get($type, $key);
-		if ( empty($result->id) ) {
-			return $returnKeyIfNotFound ? $key : '';
-		} else {
-			return $result->value;
-		}
+	private static function hasWildcard($str) {
+		return ( stripos($str, '%') !== false );
 	}
 
 
@@ -224,20 +226,27 @@ class Enum {
 	/**
 	<fusedoc>
 		<description>
-			check whether the string has SQL wildcard character
+			get value of specific enum item
 		</description>
 		<io>
 			<in>
-				<string name="$str" example="home-%" />
+				<string name="$type" />
+				<string name="$key" />
+				<boolean name="$returnKeyIfNotFound" optional="yes" default="true" comments="return empty string otherwise" />
 			</in>
 			<out>
-				<boolean name="~return~" />
+				<string name="~return~" />
 			</out>
 		</io>
 	</fusedoc>
 	*/
-	private static function __hasWildcard($str) {
-		return ( stripos($str, '%') !== false );
+	public static function value($type, $key, $returnKeyIfNotFound=true) {
+		$result = self::get($type, $key);
+		if ( empty($result->id) ) {
+			return $returnKeyIfNotFound ? $key : '';
+		} else {
+			return $result->value;
+		}
 	}
 
 
