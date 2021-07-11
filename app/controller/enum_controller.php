b39dfd97be7ed18063fa40f8a342d8918a047a4b
henrygotmojo <henrygotmojo@gmail.com>
2021-07-11 14:42:19 +0800

    apply i18n to enum-controller to show more fields while adding or editing

--- app/controller/enum_controller.php
+++ app/controller/enum_controller.php
@@ -19,17 +19,49 @@ $scaffold = array(
 	'listField' => array(
 		'id' => '60',
 		'key|type' => '15%',
-		'value|remark' => '50%',
+		call_user_func(function(){
+			$fields = array();
+			// all available locale
+			$locales = class_exists('I18N') ? I18N::localeAll() : ['en'];
+			// field with multi-language
+			foreach ( ['value','remark'] as $fieldName ) {
+				foreach ( $locales as $localeName ) {
+					if ( $localeName == 'en' ) $fields[] = $fieldName;
+					elseif ( !F::is('*.index,*.row') ) $fields[] = $fieldName.'__'.str_replace('-', '_', $localeName);
+				}
+			}
+			// done!
+			return implode('|', $fields);
+		}) => '50%',
 		'seq' => '70'
 	),
-	'fieldConfig' => array(
-		'id',
+	'fieldConfig' => array_merge([
 		'type' => array('placeholder' => true, 'readonly' => !Auth::userInRole('SUPER'), 'default' => $_SESSION['enumController__enumType']),
 		'key' => array('placeholder' => true),
-		'value' => array('placeholder' => true),
-		'remark' => array('placeholder' => true, 'format' => 'textarea', 'style' => 'height: 5rem'),
 		'seq' => array('placeholder' => true),
-	),
+	], call_user_func(function(){
+		$fields = array();
+		// all available locale
+		$locales = class_exists('I18N') ? I18N::localeAll() : ['en'];
+		// field with multi-language
+		foreach ( $locales as $localeName ) {
+			$fields[ ( $localeName == 'en' ) ? 'value' : 'value__'.str_replace('-', '_', $localeName) ] = array(
+				'label' => ( $localeName == 'en' ),
+				'placeholder' => ( count($locales) == 1 ),
+				'inline-label' => ( count($locales) > 1 ) ? ('<div style="width: 2.5rem">'.strtoupper($localeName).'</div>') : false,
+			);
+			$fields[ ( $localeName == 'en' ) ? 'remark' : 'remark__'.str_replace('-', '_', $localeName) ] = array(
+				'format' => 'textarea',
+				'label' => ( $localeName == 'en' ),
+				'placeholder' => ( count($locales) == 1 ),
+				'inline-label' => ( count($locales) > 1 ) ? ('<div style="width: 2.5rem">'.strtoupper($localeName).'</div>') : false,
+				'pre-help' => ( count($locales) > 1 and $localeName == 'en' ) ? '<small class="d-block mt-3">Remark</small>' : false,
+				'style' => 'height: 5rem',
+			);
+		}
+		// done!
+		return $fields;
+	})),
 	'writeLog' => class_exists('Log'),
 );
 
