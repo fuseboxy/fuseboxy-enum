d47ada6ba5b6c362ecaa5591d238756b4653d3c0
henrygotmojo <henrygotmojo@gmail.com>
2022-08-19 11:47:37 +0800

    add db init script

---
+++ enum.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS `enum` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(50) DEFAULT NULL,
+  `key` varchar(200) DEFAULT NULL,
+  `value` text,
+  `value__zh_hk` text,
+  `remark` text,
+  `remark__zh_hk` text,
+  `seq` int(11) DEFAULT NULL,
+  `disabled` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `type` (`type`),
+  KEY `key` (`key`),
+  KEY `disabled` (`disabled`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
